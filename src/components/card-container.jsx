d142380feb6e432aacb02d3178ecaa0d501244c3
Phil <182430206+Fico27@users.noreply.github.com>
2025-06-12 16:06:40 +0200

    added in useEffect to fetch name and pokemon images

--- src/components/card-container.jsx
+++ src/components/card-container.jsx
@@ -1,8 +1,51 @@
+import { useState, useEffect } from "react";
+
+const randomPokemon = [
+  "bulbasaur",
+  "charizard",
+  "pikachu",
+  "gengar",
+  "lucario",
+  "snorlax",
+  "dragonite",
+  "mewtwo",
+  "scizor",
+  "salamence",
+  "gardevoir",
+  "infernape",
+];
+
 function CardContainer() {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const promises = randomPokemon.map(async (name) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const data = await res.json();
+        return {
+          name: data.name,
+          image: data.sprites.other.dream_world.front_default,
+        };
+      });
+
+      const results = await Promise.all(promises);
+      console.log(results);
+      setPokemonData(results);
+    };
+    getPokemon();
+  }, []);
+
   return (
-    <>
-      <h2>I hold cards</h2>
-    </>
+    <div className="cardContainer">
+      {pokemonData.map((pokemon) => (
+        <div className="card" key={pokemon.name}>
+          <img className="img" src={pokemon.image}></img>
+          <br />
+          <span>{pokemon.name}</span>
+        </div>
+      ))}
+    </div>
   );
 }
 
